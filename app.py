e52c5c0e768a5d359029110ea75a5cc7f6712883
Bruno Costa <brunovmcosta@gmail.com>
2025-03-12 19:34:28 -0300

    Efetuada carga de dados e apresentado dataframe

--- app.py
+++ app.py
@@ -2,3 +2,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import streamlit
+
+def load_data():
+
+    return pd.read_csv('data/processed/bikes_completed.csv')
+
+df = load_data()
+
+print(df)
